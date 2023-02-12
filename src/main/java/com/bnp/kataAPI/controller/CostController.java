82fec4cb75673f3990a83acb0e22b88889149a17
Amine <boussettaamine89@gmail.com>
2023-02-12 21:24:49 +0100

    update controller to use the service layer

--- src/main/java/com/bnp/kataAPI/controller/CostController.java
+++ src/main/java/com/bnp/kataAPI/controller/CostController.java
@@ -2,17 +2,22 @@ package com.bnp.kataAPI.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bnp.kataAPI.service.CostService;
+
 @RestController
 @RequestMapping("/api")
 public class CostController {
+	@Autowired
+	CostService costService;
 
 	@GetMapping("/cost")
-	public String cost(@RequestParam List<String> title) {
-		return title.get(0);
+	public float cost(@RequestParam List<String> title) {
+		return costService.basketCost(title);
 	}
 }
