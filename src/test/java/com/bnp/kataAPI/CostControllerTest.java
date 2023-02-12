4cc0e32ce7143ac8466cded14957af2045019c83
Amine <boussettaamine89@gmail.com>
2023-02-12 19:40:32 +0100

    test api

---
+++ src/test/java/com/bnp/kataAPI/CostControllerTest.java
@@ -0,0 +1,19 @@
+package com.bnp.kataAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest
+public class CostControllerTest {
+	@Autowired
+	MockMvc mockMvc;
+
+	@Test
+	public void costTestOk() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/api/cost")).andExpect(MockMvcResultMatchers.status().isOk());
+	}
+}
