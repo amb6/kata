e0abeb6313e9bd3ab6f6361b1fe17f926baf56f5
Amine <boussettaamine89@gmail.com>
2023-02-12 19:06:34 +0100

    First commit

---
+++ src/main/java/com/bnp/kataAPI/KataApiApplication.java
@@ -0,0 +1,13 @@
+package com.bnp.kataAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KataApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bnp/kataAPI/KataApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnp.kataAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
